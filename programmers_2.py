030a317e3ee85d304b60f11cb11688ca02d17aa4
Chris Kim <kimjm010@icloud.com>
2023-03-19 22:23:48 +0900

    Python_ JadenCase 문자열 만들기

---
+++ programmers_2.py
@@ -0,0 +1,19 @@
+# Proframmers Lv.2 JadenCase 문자열 만들기
+def solution(s):
+    answer = ''
+    words = list(s.lower().split(" "))
+    result = []
+    
+    for word in words:
+        if word != " ":
+            try:
+                int(word[0])
+                result.append(word)
+            except:
+                result.append(word.title())
+        else:
+            result.append(word)
+    
+    answer = " ".join(result)
+            
+    return answer
