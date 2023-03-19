77b6740516021c511830a84fbd5df9cde6907111
Chris Kim <kimjm010@icloud.com>
2023-03-19 22:22:43 +0900

    Python_최댓값과 최솟값

---
+++ programmers_1.py
@@ -0,0 +1,18 @@
+# Proframmers Lv.2 최댓값과 최솟값
+
+def solution(s):
+    answer = ''
+    numbers = list(map(int, s.split()))
+
+    max = numbers[0]
+    min = numbers[1]
+    for i in range(0, len(numbers)):
+        if numbers[i] > max:
+            max = numbers[i]
+        elif numbers[i] < min:
+            min = numbers[i]
+
+    answer = f"{min} {max}"
+
+            
+    return answer
