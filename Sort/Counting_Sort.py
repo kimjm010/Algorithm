d3f937534dbfe76adead5ec48aa369a00e3e9b77
Chris Kim <kimjm010@icloud.com>
2023-05-03 08:44:09 +0900

    Binary_Search, Bisect

--- Sort/Counting_Sort.py
+++ Sort/Counting_Sort.py
@@ -7,10 +7,14 @@ array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
 # 모든 범위를 초함하는 리스트 선언(모든 값은 0으로 초기화)
 count = [0] * (max(array) + 1)
 
-for i in range(len(array)):
-    count[array[i]] += 1
+for i in range(len(array)): 
+    count[array[i]] += 1 # 각 데이터ㅔ 해당하는 인덱스의 값 증가
 
-for i in range(len(count)):
-    for j in range(count[i]):
-        print(i, end= " ")
-        
+for i in range(len(count)): # 리스트에 기록된 정렬 정보 확인
+    for j in range(count[i]): 
+        print(i, end= " ") # 띄어쓰기를 구분으로 등장한 횟수만큰 인덱스 출력
+
+
+# 계수 정렬의 시간 복잡도와 공간 복잡도는 모두 O(N + K)
+# 계수 정렬은 때에 따라 심각한 비효율성을 초래함 ex) 데이터가 0과 999,999로 단 2개만 존재하는 경우
+# 계수 정렬은 동일한 값을 가지는 데이터가 여러 개 등장할 때 효과적으로 사용할 수 있음 ex) 성적의 경우
